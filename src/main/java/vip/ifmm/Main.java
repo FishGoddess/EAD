defc05b8958cdf3a974e24352cdeefe6c62ca2fd
FishGoddess <1149062639@qq.com>
2019-01-08 00:07:11 +0800

    基本功能完成，准备网络模块

--- src/main/java/vip/ifmm/Main.java
+++ src/main/java/vip/ifmm/Main.java
@@ -15,13 +15,31 @@ import vip.ifmm.executor.vcs.VCSExecutor;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        VCSExecutor git = Environment.getVCSExecutor();
-        git.cloneProject();
-        git.updateProject();
+    // 执行器
+    private static final VCSExecutor git = Environment.getVCSExecutor();
+    private static final BuildExecutor mvn = Environment.getBuildExecutor();
 
-        BuildExecutor mvn = Environment.getBuildExecutor();
-        mvn.packageProject();
-        mvn.deployProject();
+    // 初始化项目
+    private static final String INIT = "init";
+
+    // 更新项目
+    private static final String UPDATE = "update";
+
+    public static void main(String[] args) {
+
+        //init();
+        update();
+    }
+
+    // 初始化项目
+    private static boolean init() {
+        // 只有前一个步骤执行成功才继续执行
+        return git.cloneProject() && mvn.packageProject() && mvn.deployProject();
+    }
+
+    // 更新项目
+    private static boolean update() {
+        // 只有前一个步骤执行成功才继续执行
+        return git.updateProject() && mvn.packageProject() && mvn.deployProject();
     }
 }
